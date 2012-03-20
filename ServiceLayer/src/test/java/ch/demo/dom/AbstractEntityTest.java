a5f04ddcfd279bdcfdef4405fd8f772b17332cd4
steve.hostettler <steve.hostettler@e75a912d-c469-4da3-42e7-869463994cc8>
2012-03-20 16:25:27 +0000

    improve testability

--- ServiceLayer/src/test/java/ch/demo/dom/AbstractEntityTest.java
+++ ServiceLayer/src/test/java/ch/demo/dom/AbstractEntityTest.java
@@ -3,11 +3,15 @@
  */
 package ch.demo.dom;
 
+import java.sql.Connection;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import org.apache.derby.tools.ij;
+import org.dbunit.DataSourceDatabaseTester;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
@@ -17,14 +21,21 @@ import org.eclipse.persistence.internal.jpa.EntityManagerImpl;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Abstracts the entity tests.
+ * 
  * @author hostettler
  * 
  */
 public abstract class AbstractEntityTest {
-    
+
+    /** The default logger for the class. */
+    public static final Logger LOGGER = LoggerFactory
+            .getLogger(DataSourceDatabaseTester.class);
+
     /** The factory that produces entity manager. */
     private static EntityManagerFactory mEmf;
     /** The entity manager that persists and queries the DB. */
@@ -32,29 +43,42 @@ public abstract class AbstractEntityTest {
     /** A transaction. */
     private static EntityTransaction mTrx;
     /** Connection to the database. */
-    private static IDatabaseConnection mConnection;
+    private static IDatabaseConnection mDBUnitConnection;
     /** Test dataset. */
     private static IDataSet mDataset;
 
+    /** script to clean up and prepare the DB. */
+    private static String mDDLFileName = "/sql/createStudentsDB_DERBY.sql";
+    /** Target persistence unit. */
+    private static String mPersistenceUnit = "JEE6Demo-Test-Persistence";
+
     /**
      * Initializes the manager and the database.
-     * @throws Exception if anything goes wrong.
+     * 
+     * @throws Exception
+     *             if anything goes wrong.
      */
     @BeforeClass
     public static void initEntityManager() throws Exception {
         // Get the entity manager for the tests.
-        mEmf = Persistence
-                .createEntityManagerFactory("JEE6Demo-Test-Persistence");
+        mEmf = Persistence.createEntityManagerFactory(mPersistenceUnit);
         mEntityManager = mEmf.createEntityManager();
 
-        // Load the test datasets in the database
-        mConnection = new DatabaseConnection(
-                ((EntityManagerImpl) (mEntityManager.getDelegate())).getServerSession()
-                        .getAccessor().getConnection());
 
+        Connection connection = ((EntityManagerImpl) (mEntityManager
+                .getDelegate())).getServerSession().getAccessor()
+                .getConnection();
+
+        ij.runScript(connection,
+                AbstractEntityTest.class.getResourceAsStream(mDDLFileName),
+                "UTF-8", System.out, "UTF-8");
+
+        // Load the test datasets in the database
+        mDBUnitConnection = new DatabaseConnection(connection);
         mDataset = new FlatXmlDataSetBuilder().build(Thread.currentThread()
                 .getContextClassLoader()
                 .getResourceAsStream("students-datasets.xml"));
+
     }
 
     /**
@@ -68,14 +92,17 @@ public abstract class AbstractEntityTest {
 
     /**
      * Prepares a new test.
-     * @throws Exception if anything goes wrong.
+     * 
+     * @throws Exception
+     *             if anything goes wrong.
      */
     @Before
     public void initTransaction() throws Exception {
+        LOGGER.info("Prepare the database.");
+        DatabaseOperation.CLEAN_INSERT.execute(mDBUnitConnection, mDataset);
+        LOGGER.info("Start a new transaction for the test");
         mTrx = mEntityManager.getTransaction();
-        DatabaseOperation.CLEAN_INSERT.execute(mConnection, mDataset);
     }
-    
 
     /**
      * @return the emf
@@ -102,7 +129,7 @@ public abstract class AbstractEntityTest {
      * @return the connection
      */
     protected static final IDatabaseConnection getConnection() {
-        return mConnection;
+        return mDBUnitConnection;
     }
 
     /**
